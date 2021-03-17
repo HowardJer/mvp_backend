2830c4439e311421bb7d2167e9751ab4d6f41d88
Howard <howardjer@gmail.com>
2021-03-16 21:05:11 -0600

    copied 1st full stack to try to set up mvp backend

---
+++ app.py
@@ -0,0 +1,144 @@
+from flask import Flask, request, Response
+# from flask_cors import CORS
+import mariadb
+import dbcreds
+import json
+import sys
+
+app = Flask(__name__)
+# CORS(app)
+
+
+
+def connect(): 
+    return mariadb.connect(
+        host = dbcreds.host,
+        port = dbcreds.port,
+        user = dbcreds.user,
+        password = dbcreds.password,
+        database = dbcreds.database 
+    )
+
+@app.route("/members",methods=["GET", "POST", "PATCH", "DELETE"])  
+def member():
+    if request.method == "GET":
+        conn = None
+        cursor = None
+        results = None
+        try:
+            conn = connect()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM members")
+            results = cursor.fetchall()
+        except Exception as e:
+            print(e)
+        finally: 
+            if(conn != None):
+                conn.rollback() 
+                conn.close() 
+            if(cursor != None):
+                cursor.close()
+            if(results != None or results == []):
+                members = []
+                for item in results:
+                    member ={
+                        "id":item[0],
+                        "member_name":item[1]
+                        # "password":item[1]
+                    }              
+                    members.append(member)
+                return Response(
+                    json.dumps(member, default=str),
+                    mimetype="application/json",
+                    status=200)
+            else: 
+                return Response("Failed", mimetype="html/text", status=200) #400           
+    # if request.method == "POST":
+    #     content = request.json.get("content")
+    #     conn = None
+    #     cursor = None
+    #     results = None
+    #     try:
+    #         conn = connect()
+    #         cursor = conn.cursor()
+    #         cursor.execute("INSERT INTO blog_list (content) VALUES (?)",
+    #                        [content]
+    #         )
+    #         conn.commit()
+    #         blog_id = cursor.lastrowid
+    #     except Exception as e:
+    #         print(e)
+    #     finally: 
+    #         if(conn != None):
+    #             conn.rollback() 
+    #             conn.close() 
+    #         if(cursor != None):
+    #             cursor.close()
+    #         if(blog_id != None ):
+
+
+    #             blog ={
+    #                 "id":blog_id,
+    #                 "content": content
+    #             }              
+
+    #             return Response(
+    #                 json.dumps(blog, default=str),
+    #                 mimetype="application/json", status=200 #200
+    #             )
+    #         else: 
+    #             return Response(
+    #                 "Failed", 
+    #                 mimetype="html/text", status=200 #400
+    #             )            
+    # if request.method == "PATCH":
+    #     content = request.json.get("content")
+    #     blog_id = request.json.get("id")
+    #     conn = None
+    #     cursor = None
+    #     try:
+    #         conn = connect()
+    #         cursor = conn.cursor()
+    #         cursor.execute("UPDATE blog_list SET  content =? WHERE id=?",
+    #                        [content, blog_id])
+    #         conn.commit()
+    #         rowcount = cursor.rowcount            
+    #     except Exception as e:
+    #         print(e)
+    #     finally: 
+    #         if(conn != None):
+    #             conn.rollback() 
+    #             conn.close() 
+    #         if(cursor != None):
+    #             cursor.close()
+    #         if(rowcount == 1):
+    #             blog ={
+    #                 "id":blog_id,
+    #                 "content": content
+    #             }              
+    #             return Response(json.dumps(blog, default=str),
+    #                             mimetype="application/json",status=200)  
+    #         else: 
+    #             return Response("Failed", mimetype="html/text", status=200)   
+    #     blog_id = request.json.get("id")
+    #     conn = None
+    #     cursor = None
+    #     try:
+    #         conn = connect()
+    #         cursor = conn.cursor()
+    #         cursor.execute("DELETE FROM blog_list WHERE id=?",[blog_id])
+    #         rows = cursor.rowcount
+    #         conn.commit()
+    #         rowcount = cursor.rowcount
+    #     except Exception as e:
+    #         print(e)
+    #     finally: 
+    #         if(conn != None):
+    #             conn.rollback() 
+    #             conn.close() 
+    #         if(cursor != None):
+    #             cursor.close()
+    #         if(rowcount == 1): 
+
+    #             return Response("Success",mimetype="html/text",status=200)  
+    #             return Response("Failed", mimetype="html/text", status=200)                        
