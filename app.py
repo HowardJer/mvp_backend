cc47e3987e4883cd4f8ba3fa79412b6910bff81f
Howard <howardjer@gmail.com>
2021-03-20 16:23:07 -0600

    completed post working on patch

--- app.py
+++ app.py
@@ -39,25 +39,83 @@ def book():
                 cursor.close()
             if(results != None or results == []):
                 library = []
-            return Response(
+                return Response(
                     json.dumps(results, default=str),
                     mimetype="application/json",
                     status=200)
             else: 
                 return Response("Failed", mimetype="html/text", status=400)         
     if request.method == "POST":
-        content = request.json.get("content")
+        library_id = request.json.get("id")
+        member_id = request.json.get("member_id")
+        book_or_tape = request.json.get("book_or_tape")
+        author_first_name = request.json.get("author_first_name")
+        author_last_name = request.json.get("author_last_name")
+        book_title = request.json.get("book_title")
+        genre = request.json.get("genre")
+              
         conn = None
         cursor = None
         results = None
         try:
             conn = connect()
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO blog_list (content) VALUES (?)",
-                           [content]
+            cursor.execute("INSERT INTO library (member_id, book_or_tape, author_first_name, author_last_name, book_title, genre) VALUES (?, ?, ?, ?, ?, ?)",
+                           [member_id, book_or_tape, author_first_name, author_last_name, book_title, genre]
             )
             conn.commit()
-            blog_id = cursor.lastrowid
+            library_id = cursor.lastrowid
+        except Exception as e:
+            print(e)
+        finally:
+            print(results)
+            if(conn != None):
+                conn.rollback() 
+                conn.close() 
+            if(cursor != None):
+                cursor.close()
+            if(library_id != None ):
+
+
+                book ={
+                    "id":library_id,
+                    "member_id": member_id,
+                    "book_or_tape": book_or_tape,
+                    "author_first_name": author_first_name,
+                    "author_last_name": author_last_name,
+                    "boot_title": book_title,
+                    "genre": genre          
+                }              
+
+                return Response(
+                    json.dumps(book, default=str),
+                    mimetype="application/json", status=200 
+                )
+            else: 
+                return Response(
+                    "Failed", 
+                    mimetype="html/text", status=200 
+                )   
+                         
+    if request.method == "PATCH":
+        library_id = request.json.get("id")
+        member_id = request.json.get("member_id")
+        book_or_tape = request.json.get("book_or_tape")
+        author_first_name = request.json.get("author_first_name")
+        author_last_name = request.json.get("author_last_name")
+        book_title = request.json.get("book_title")
+        genre = request.json.get("genre")
+        
+        conn = None
+        cursor = None
+        try:
+            conn = connect()
+            cursor = conn.cursor()
+            cursor.execute("UPDATE library SET book_or_tape =?, author_first_name =?, author_last_name =?, book_title =?, genre =? WHERE id=?",
+                           [book_or_tape, author_first_name, author_last_name, book_title, genre, library_id])
+            rows = cursor.rowcount
+            conn.commit()
+            rowcount = cursor.rowcount            
         except Exception as e:
             print(e)
         finally: 
@@ -66,52 +124,27 @@ def book():
                 conn.close() 
             if(cursor != None):
                 cursor.close()
-            if(blog_id != None ):
-
-
-                blog ={
-                    "id":blog_id,
-                    "content": content
-                }              
-
-                return Response(
-                    json.dumps(blog, default=str),
-                    mimetype="application/json", status=200 #200
-                )
+            if(rowcount == 1):
+                book ={
+                    "id":library_id,
+                    "member_id": member_id,
+                    "book_or_tape": book_or_tape,
+                    "author_first_name": author_first_name,
+                    "author_last_name": author_last_name,
+                    "boot_title": book_title,
+                    "genre": genre          
+                }                   
+                return Response(json.dumps(book, default=str),
+                                mimetype="application/json",status=200)  
             else: 
-                return Response(
-                    "Failed", 
-                    mimetype="html/text", status=200 #400
-                )            
-    # if request.method == "PATCH":
-    #     content = request.json.get("content")
-    #     blog_id = request.json.get("id")
-    #     conn = None
-    #     cursor = None
-    #     try:
-    #         conn = connect()
-    #         cursor = conn.cursor()
-    #         cursor.execute("UPDATE blog_list SET  content =? WHERE id=?",
-    #                        [content, blog_id])
-    #         conn.commit()
-    #         rowcount = cursor.rowcount            
-    #     except Exception as e:
-    #         print(e)
-    #     finally: 
-    #         if(conn != None):
-    #             conn.rollback() 
-    #             conn.close() 
-    #         if(cursor != None):
-    #             cursor.close()
-    #         if(rowcount == 1):
-    #             blog ={
-    #                 "id":blog_id,
-    #                 "content": content
-    #             }              
-    #             return Response(json.dumps(blog, default=str),
-    #                             mimetype="application/json",status=200)  
-    #         else: 
-    #             return Response("Failed", mimetype="html/text", status=200)   
+                return Response("Failed", mimetype="html/text", status=200)   
+    
+    
+    
+    
+    
+    # if request.method == "DELETE":
+    
     #     blog_id = request.json.get("id")
     #     conn = None
     #     cursor = None
